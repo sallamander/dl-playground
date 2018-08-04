cf29a99d0e8ca763d3abd42406cc9a8860088187
Sean Sall <sean@arterys.com>
2018-08-03 19:51:05 -0700

    Update command line argument functionality in building Docker images

--- setup/build_docker_images.sh
+++ setup/build_docker_images.sh
@@ -12,14 +12,14 @@ function usage {
     echo "                                       to 4.5.1, which is compatible with the repository "
     echo "                                       YMLs."
     echo ""
+    echo "    --docker_user                      Username for the Docker images. "
+    echo ""
     echo "    --fpath_custom_dockerfile fpath:   Build a custom Docker image from a provided "
     echo "                                       Dockerfile after the Dockerfile.base and "
     echo "                                       Dockerfile image builds, meant to allow for "
     echo "                                       further customization of a user's environment. "
     echo "                                       This Dockerfile must use dl-playground/final as "
     echo "                                       the base image."
-    echo ""
-    echo "    --user                             Username for the Docker images. "
     exit 1
 }
 
@@ -28,15 +28,14 @@ do
     case "$1" in
             --conda_version)
                 CONDA_VERSION=$2
-                echo $CONDA_VERSION
                 shift 2
                 ;;
             --fpath_custom_dockerfile)
                 FPATH_CUSTOM_DOCKERFILE=$2
                 shift 2
                 ;;
-            --user)
-                USER=$2
+            --docker_user)
+                DOCKER_USER=$2
                 shift 2
                 ;;
             --help)
@@ -46,9 +45,20 @@ do
     esac
 done
 
-docker build --build-arg user=$USER --build-arg conda_version=$CONDA_VERSION -t dl-playground/base --file ./Dockerfile.base ./
-docker build --build-arg user=$USER --build-arg conda_version=$CONDA_VERSION -t dl-playground/final --file ./Dockerfile ./
+if [ -z "$DOCKER_USER" ]; then
+    echo "--docker_user flag must be specified!"
+    exit 1
+fi
+
+if [ -z "$CONDA_VERSION" ]; then
+    echo "--conda_version not specified, using the default of 4.5.1."
+    CONDA_VERSION=4.5.1
+fi
+
+echo "Creating images with docker username $DOCKER_USER and miniconda version $CONDA_VERSION..."
+docker build --build-arg user=$DOCKER_USER --build-arg conda_version=$CONDA_VERSION -t dl-playground/base --file ./Dockerfile.base ./
+docker build --build-arg user=$DOCKER_USER --build-arg conda_version=$CONDA_VERSION -t dl-playground/final --file ./Dockerfile ./
 if [ ! -z "$FPATH_CUSTOM_DOCKERFILE" ]; then
-    echo "Building custom Docker image based off of $fpath_custom_dockerfile..."
-    docker build --build-arg user=$USER -t dl-playground/custom --file $FPATH_CUSTOM_DOCKERFILE ./
+    echo "Building custom Docker image based off of $FPATH_CUSTOM_DOCKERFILE ..."
+    docker build --build-arg user=$DOCKER_USER -t dl-playground/custom --file $FPATH_CUSTOM_DOCKERFILE ./
 fi
