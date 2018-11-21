c468afba3e6f63279067d08ad1c5b88c5a3b230e
Sean Sall <sean@arterys.com>
2018-11-21 21:56:03 +0000

    Remove unused functions from datasets.ops

--- dl_playground/datasets/ops.py
+++ dl_playground/datasets/ops.py
@@ -26,25 +26,6 @@ def apply_op(map_op_fn, sample, sample_keys, map_op_fn_kwargs=None):
     return sample
 
 
-def center_image(image, label):
-    """Apply tf.image.per_image_standardization to `image`
-
-    Note that `label` remains unchanged.
-
-    :param image: image to center
-    :type image: tensorflow.Tensor
-    :param label: label to pass through
-    :type label: tensorflow.Tensor
-    :return: tensorflow.Tensor objects holding the centered image and
-     unmodified label
-    :rtype: tuple(tensorflow.Tensor)
-    """
-
-    image = tf.image.per_image_standardization(image)
-
-    return image, label
-
-
 def format_batch(batch, input_keys, target_keys):
     """Format the batch from a single dictionary into a tuple of dictionaries
 
@@ -62,57 +43,6 @@ def format_batch(batch, input_keys, target_keys):
     inputs = {input_key: batch[input_key] for input_key in input_keys}
     targets = {target_key: batch[target_key] for target_key in target_keys}
     return (inputs, targets)
-
-
-def load_image(fpath_image, label):
-    """Parse /decode and return the provided image
-
-    This op is intended to be used on a tf.Dataset object that is built around
-    image filepaths as the inputs and class labels as the targets, where the
-    image filepaths point to 2 dimensional RGB images (i.e. of shape (height,
-    width, 3)).
-
-    :param fpath_image: filepath to the input image to parse
-    :type fpath_image: str
-    :param label: class label associated with an image; this will simply be
-     passed through this function
-    :type label: tensorflow.Tensor
-    :return: tensorflow.Tensor objects holding the parsed / decoded image
-     along with the class label
-    :rtype: tuple(tensorflow.Tensor)
-    """
-
-    image_string = tf.read_file(fpath_image)
-    image = tf.image.decode_image(image_string, channels=3)
-
-    # set_shape because `tf.image.decode_image` does not set the shape, and if
-    # it isn't set then tf.image_resize_images won't work downstream
-    image.set_shape([None, None, None])
-    image = tf.image.convert_image_dtype(image, tf.float32)
-
-    return image, label
-
-
-def reshape_image_and_label(image, label, target_image_shape,
-                            num_label_classes):
-    """Reshape image to target_image_shape and one-hot encode label
-
-    :param image: image to reshape
-    :type image: tensorflow.Tensor
-    :param label: label to reshape
-    :type label: tensorflow.Tensor
-    :param target_image_shape: (height, width) to resize `image` to
-    :type target_image_shape: tuple or list
-    :param num_label_classes: `dense` argument to pass to `tensorflow.one_hot`
-    :type num_label_classes: int
-    :return: tensorflow.Tensor objects holding the reshaped image and label
-    :rtype: tuple(tensorflow.Tensor)
-    """
-
-    image = tf.image.resize_images(image, target_image_shape)
-    label = tf.one_hot(label, num_label_classes)
-
-    return image, label
 
 
 def resize_images(images, size):
--- tests/unit_tests/datasets/test_ops.py
+++ tests/unit_tests/datasets/test_ops.py
@@ -1,18 +1,12 @@
 """Unit tests for datasets.ops"""
 
-import os
-import tempfile
 from unittest.mock import MagicMock
 
-import imageio
 import numpy as np
 import pytest
 import tensorflow as tf
 
-from datasets.ops import (
-    apply_op, center_image, format_batch, load_image, reshape_image_and_label,
-    resize_images
-)
+from datasets.ops import apply_op, format_batch, resize_images
 
 
 @pytest.fixture(scope='module')
@@ -41,24 +35,6 @@ def label():
 
     label = tf.constant(1, dtype=tf.uint8)
     return label
-
-
-def test_center_image(image, label):
-    """Test center_image
-
-    :param image: module wide image object fixture
-    :type image: tensorflow.Tensor
-    :param label: module wide label object fixture
-    :type label: tensorflow.Tensor
-    """
-
-    image_centered_op, label = center_image(image, label)
-
-    with tf.Session() as sess:
-        image_centered = sess.run(image_centered_op)
-
-        assert np.allclose(image_centered.mean(), 0, atol=1e-4)
-        assert np.allclose(image_centered.std(), 1, atol=1e-4)
 
 
 def test_format_batch():
@@ -99,68 +75,6 @@ def test_format_batch():
     assert np.array_equal(formatted_batch[0]['labels'], labels)
     assert np.array_equal(formatted_batch[1]['images2'], images2)
     assert np.array_equal(formatted_batch[1]['labels2'], labels2)
-
-
-def test_load_image():
-    """Test load_image"""
-
-    tempdir = tempfile.TemporaryDirectory()
-
-    fpath_jpg = os.path.join(tempdir.name, 'image.jpg')
-    fpath_png = os.path.join(tempdir.name, 'image.png')
-
-    height, width = np.random.randint(128, 600, 2)
-    num_channels = 3
-    image = np.random.random((height, width, num_channels)).astype(np.uint8)
-    imageio.imwrite(fpath_jpg, image)
-    imageio.imwrite(fpath_png, image)
-
-    for fpath_image in [fpath_jpg, fpath_png]:
-        with tf.device('/cpu:0'):
-            loaded_image_op, label_op = load_image(
-                fpath_image, label=tf.constant(1)
-            )
-
-        with tf.Session() as sess:
-            loaded_image = sess.run(loaded_image_op)
-            label = sess.run(label_op)
-
-            assert np.allclose(loaded_image, image)
-            assert label == 1
-
-
-def test_reshape_image_and_label(image, label):
-    """Test reshape_image_and_label
-
-    :param image: module wide image object fixture
-    :type image: tensorflow.Tensor
-    :param label: module wide label object fixture
-    :type label: tensorflow.Tensor
-    """
-
-    sess = tf.Session()
-    test_cases = [
-        {'target_image_shape': (64, 64), 'num_label_classes': 1},
-        {'target_image_shape': [28, 32], 'num_label_classes': 3},
-        {'target_image_shape': (128, 64), 'num_label_classes': 1000},
-        {'target_image_shape': [256, 256], 'num_label_classes': 10},
-    ]
-    for test_case in test_cases:
-        target_image_shape = test_case['target_image_shape']
-        num_label_classes = test_case['num_label_classes']
-        with tf.device('/cpu:0'):
-            image_reshaped_op, label_reshaped_op = reshape_image_and_label(
-                image, label, target_image_shape, num_label_classes
-            )
-
-        image_reshaped = sess.run(image_reshaped_op)
-        label_reshaped = sess.run(label_reshaped_op)
-
-        expected_image_shape = tuple(target_image_shape) + (3, )
-        assert np.allclose(image_reshaped.shape, expected_image_shape)
-        assert np.allclose(label_reshaped.shape, (num_label_classes,))
-
-    sess.close()
 
 
 def test_resize_images():
